bededd12c59aa9da3a35dcbf10a466ef45a90188
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2022-10-17 11:31:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+/* cria o servidor */
+var fs = require('fs'); 
+var http = require("http"); 
+var express = require('express'); 
+var app = express();
+var bodyParser = require('body-parser'); 
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var servidor = app.listen(8080, function() {
+    var porta = servidor.address().port;
+console.log("Servidor executando na porta %s", porta);
+ });
+
+
+  app.get('/', function (req, res) {
+fs.readFile('index.html', function(erro, dado) { 
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+    res.write(dado); res.end();
+}); 
+});
+app.post('/profile', urlencodedParser, function (req, res){ 
+    fs.readFile('profile.html', function(erro, dado) { var hoje = new Date(); var valores = {
+        'nome': req.body.nome,
+        'anonasc': req.body.anonasc, 
+        'idade': (hoje.getFullYear() - parseInt(req.body.anonasc)) }; 
+        for (var chave in valores) { 
+            dado = dado.toString().replace("{{" + chave + "}}", valores[chave]); 
+        }
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write(dado); 
+res.end();
+}); 
+});
